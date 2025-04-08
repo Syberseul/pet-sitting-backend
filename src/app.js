18643ae24109e4375323f2d8e83619f75d92c79a
Yiteng Huang <hyy528924543@gmail.com>
2025-04-09 08:49:21 +1000

    cors check

--- src/app.js
+++ src/app.js
@@ -5,9 +5,19 @@ const createError = require("http-errors");
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://pet-sitting-family.vercel.app", "http://localhost:5137"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use("/", require("./router/index"));
+
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use((req, res, next) => {
   next(createError(404));
