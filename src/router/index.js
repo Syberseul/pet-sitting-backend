d32d02d62e064fb481eb97e32fa42b005715ebb9
Yiteng Huang <hyy528924543@gmail.com>
2025-04-10 20:22:48 +1000

    wx add leagal request list

--- src/router/index.js
+++ src/router/index.js
@@ -7,4 +7,9 @@ const dogRouter = require("./dogRouter");
 router.use("/users", userRouter);
 router.use("/dogs", dogRouter);
 
+const wxValidationContent = "80954e3d9f517a44a6173939ffa17a55";
+router.get("/Q3aVw9J6cN.txt", (req, res) => {
+  res.status(200).send(wxValidationContent);
+});
+
 module.exports = router;
